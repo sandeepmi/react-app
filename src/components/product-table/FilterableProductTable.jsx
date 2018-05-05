a0cf33e03de52f678f06bfdeea58fef397f83bba
Mattaparthi, Sandeep <Sandeep.Mattaparthi@ampf.com>
2018-05-05 11:53:02 -0500

    search form state and events

--- src/components/product-table/FilterableProductTable.jsx
+++ src/components/product-table/FilterableProductTable.jsx
@@ -3,18 +3,33 @@ import SearchBar from './SearchBar'
 import ProductTable from './ProductTable'
 
 class FilterableProductTable extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.state = {
+      search: {
+        searchText: '',
+        inStockOnly: false
+      },
       filteredProducts: this.props.products
     }
+    this.handleSearchChange = this.handleSearchChange.bind(this)
+  }
+
+  handleSearchChange (key, value) {
+    this.setState(prevState => ({
+      search: Object.assign(prevState.search, {
+        [key]: value
+      })
+    }))
   }
 
   render() {
+    const { search, filteredProducts } = this.state
+
     return (
       <div className="container">
-        <SearchBar />
-        <ProductTable products={this.state.filteredProducts} />
+        <SearchBar search={search} onChange={this.handleSearchChange} />
+        <ProductTable products={filteredProducts} search={search} />
       </div>
     )
   }
--- src/components/product-table/SearchBar.jsx
+++ src/components/product-table/SearchBar.jsx
@@ -3,18 +3,25 @@ import React, { Component } from 'react'
 class SearchBar extends Component {
   constructor (props) {
     super(props)
-    this.state = {
-      searchText: '',
-      showInStock: false
-    }
+    this.handleInputChange = this.handleInputChange.bind(this)
+  }
+
+  handleInputChange (event) {
+    const target = event.target
+    const value = target.type === 'checkbox' ? target.checked : target.value
+    const name = target.name
+
+    this.props.onChange(name, value)
   }
 
   render() {
+    const { searchText, inStockOnly } = this.props.search
+
     return (
       <div className="search-form">
         <form>
-          <input type="text" name="searchText" /><br />
-          <input type="checkbox" name="showInStock" />
+          <input type="text" name="searchText" value={searchText} onChange={this.handleInputChange} /><br />
+          <input type="checkbox" name="inStockOnly" value={inStockOnly} onChange={this.handleInputChange} />
           <label>Only show products in stock</label>
         </form>
       </div>
