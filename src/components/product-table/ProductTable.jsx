96232441ba703e89966a7526fb579747d72f981e
Mattaparthi, Sandeep <Sandeep.Mattaparthi@ampf.com>
2018-05-05 11:31:43 -0500

    show grouped products by category

--- src/components/product-table/ProductTable.jsx
+++ src/components/product-table/ProductTable.jsx
@@ -3,15 +3,39 @@ import ProductCategoryRow from './ProductCategoryRow'
 import ProductRow from './ProductRow'
 
 class ProductTable extends Component {
+  groupProducts (products) {
+    const grouped = {}
+
+    products.forEach(product => {
+      const category = product.category
+      if (!grouped.hasOwnProperty(category)) {
+        grouped[category] = []
+      }
+      grouped[category].push(product)
+    })
+
+    return grouped
+  }
+
   render() {
+    const grouped = this.groupProducts(this.props.products)
+
     return (
       <div>
         <div className="row bold">
           <div className="col">Name</div>
           <div className="col">Price</div>
         </div>
-        <ProductCategoryRow category={this.props.category} />
-        <ProductRow product={this.props.products[0]} />
+
+        {Object.keys(grouped).map(key => (
+          <div key={key}>
+            <ProductCategoryRow category={key} />
+
+            {grouped[key].map(product => (
+              <ProductRow product={product} key={product.name} />
+            ))}
+          </div>
+        ))}
       </div>
     )
   }
