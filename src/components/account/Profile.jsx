50247c4374cf7fd01a6de889610c4debce59fd64
Mattaparthi, Sandeep <Sandeep.Mattaparthi@ampf.com>
2018-05-30 09:18:50 -0500

    loading and message states for profile

--- src/components/account/Profile.jsx
+++ src/components/account/Profile.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
+import { CSSTransitionGroup } from 'react-transition-group'
 import { delay, cancelDelayedAction, getErrorMsg } from "../../helpers"
 import { getUserProfile } from "../../services/userService"
+import { Loading, Alert } from "../core"
 
 class Profile extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      profile: {},
+      profile: null,
       isLoading: false,
       message: ''
     }
@@ -30,15 +32,32 @@ class Profile extends Component {
     this.getProfile()
   }
 
+  ProfileInfo ({ profile }) {
+    return (
+      <div>
+        <p>First Name: {profile.firstName}</p>
+        <p>Last Name: {profile.lastName}</p>
+        <p>Email: {profile.email}</p>
+      </div>
+    )
+  }
+
   render() {
-    const { profile } = this.state
+    const { profile, isLoading, message } = this.state
 
     return (
       <div className="container">
         <h1 className="mt-3">Profile</h1>
-        <p>First Name: {profile.firstName}</p>
-        <p>Last Name: {profile.lastName}</p>
-        <p>Email: {profile.email}</p>
+
+        <CSSTransitionGroup transitionName="fade" transitionEnterTimeout={250} transitionLeaveTimeout={0}>
+          {isLoading ? (
+            <Loading type="card" key="loading" />
+          ) : profile ? (
+            <this.ProfileInfo profile={profile} />
+          ) : (
+            <Alert key="alert">{message}</Alert>
+          )}
+        </CSSTransitionGroup>
       </div>
     )
   }
